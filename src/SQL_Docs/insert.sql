18e4cf00418a88e1a852b99e6a91b16e39021186
yeniayi13 <115005952+yeniayi13@users.noreply.github.com>
2023-01-18 02:12:51 -0400

    Add files via upload

--- src/SQL_Docs/insert.sql
+++ src/SQL_Docs/insert.sql
@@ -7341,3 +7341,18 @@ insert into Apuesta VALUES (default, 45, 2, '2020-12-14', 2, null, 29553823,20);
 insert into Apuesta VALUES (default, 45, 3, '2020-12-15', 2, null, 29553823,1);
 insert into Apuesta VALUES (default, 45, 4, '2020-12-18', 3, null, 29551908,2);
 insert into Apuesta VALUES (default, 45, 5, '2020-12-20', 3, null, 29583211,2);
+
+
+insert into detalle_apuesta VALUES (default,2,45.00,1, 5)
+insert into detalle_apuesta VALUES (default,1,45.00,2, 8)
+insert into detalle_apuesta VALUES (default,1,45.00,3, 5)
+insert into detalle_apuesta VALUES (default,1,45.00,4, 10)
+insert into detalle_apuesta VALUES (default,1,45.00,5, 15)
+insert into detalle_apuesta VALUES (default,1,45.00,6, 20)
+insert into detalle_apuesta VALUES (default,1,45.00,7, 30)
+insert into detalle_apuesta VALUES (default,1,45.00,8, 35)
+insert into detalle_apuesta VALUES (default,1,45.00,9, 45)
+insert into detalle_apuesta VALUES (default,1,45.00,10, 55)
+
+
+
